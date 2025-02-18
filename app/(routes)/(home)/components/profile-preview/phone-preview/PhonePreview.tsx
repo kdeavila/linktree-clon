b368a24ada2d0d0838d65028892de59b013393b8
Keyner de Avila <kddeavila3@gmail.com>
2025-02-18 15:03:57 -0500

    feat: update PhonePreview.tsx (text's styles)

--- app/(routes)/(home)/components/profile-preview/phone-preview/PhonePreview.tsx
+++ app/(routes)/(home)/components/profile-preview/phone-preview/PhonePreview.tsx
@@ -7,7 +7,7 @@ export const PhonePreview = () => {
 
     return (
         <div className="mt-4 mx-auto w-full">
-            <div className="relative rounded-[40px] w-full max-w-[364px] aspect-[9/16] border-[10px] border-neutral-950 bg-neutral-950 overflow-hidden shadow-2xl mx-auto">
+            <div className="relative rounded-[40px] w-full max-w-[330px] aspect-[9/16] border-[10px] border-neutral-950 bg-neutral-950 overflow-hidden shadow-2xl mx-auto">
                 <div className="absolute inset-0">
                     {user?.backgroundImage ? (
                         <Image
@@ -22,7 +22,7 @@ export const PhonePreview = () => {
                 </div>
 
                 <div className="relative z-10 w-full h-full flex flex-col items-center text-neutral-100 text-center p-6 pt-12 phone">
-                    <div className="relative size-28 overflow-hidden rounded-full border-4 border-neutral-100 shadow-md shrink-0">
+                    <div className="relative size-24 overflow-hidden rounded-full border-4 border-neutral-100 shadow-md shrink-0">
                         <Image
                             src={user?.avatarUrl || "https://yu88eqe5p0.ufs.sh/f/Dk6Z3495qeoEFGWzdThdfcCSj8pVvGOEsAbzui56mWXk29r4"}
                             alt="User Avatar"
@@ -33,11 +33,11 @@ export const PhonePreview = () => {
                     </div>
 
                     <div className="mt-2 mb-2">
-                        <h2 className="text-2xl max-w-full font-semibold text-balance text-slate-50/90 md:text-3xl">{user?.name || 'Name profile'}</h2>
-                        <div className="flex flex-row gap-2 items-center justify-center">
-                            <p className="text-lg text-red-100/90">@{user?.username || 'username'}</p>
+                        <h2 className="text-xl max-w-full font-semibold text-balance text-slate-50/90 md:text-2xl">{user?.name || 'Name profile'}</h2>
+                        <div className="flex flex-row gap-2 items-center justify-center text-base">
+                            <p className=" text-red-100/90">@{user?.username || 'username'}</p>
                             <span>-</span>
-                            <p className="text-lg text-yellow-100/90">{user?.typeUser || 'position'}</p>
+                            <p className=" text-yellow-100/90">{user?.typeUser || 'position'}</p>
                         </div>
                     </div>
 
@@ -49,7 +49,9 @@ export const PhonePreview = () => {
                 </div>
 
 
-                <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-36 h-6 bg-neutral-950 rounded-b-xl z-20"></div>
+                <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-36 h-6 bg-neutral-950 rounded-b-xl z-20">
+                    <div className="absolute top-0 right-0 -translate-x-4 transform size-4 bg-neutral-900 rounded-full z-30"></div>
+                </div>
             </div>
         </div>
     )
